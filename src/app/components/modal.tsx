7e5ae7f502bcd3fb492929e867ef217c93e845ef
Joseph Izang <josephizang@gmail.com>
2024-09-01 01:14:37 +0100

    remove the use of useRouter temporarily. fix env.js

--- src/app/components/modal.tsx
+++ src/app/components/modal.tsx
@@ -1,10 +1,9 @@
 'use client'
 
 import React from 'react'
-import { useRouter } from 'next/router'
 
 export default function ModalForm () {
-  const router = useRouter()
+
   return (
 <>
 <div id="crud-modal" tabIndex={-1} aria-hidden="true" className="hidden overflow-y-auto overflow-x-hidden fixed top-0 right-0 left-0 z-50 justify-center items-center w-full md:inset-0 h-[calc(100%-1rem)] max-h-full bg-gradient-to-b from-[#76129b] to-[#15162c]">
